21c2633216afd32e2c4c6b3d62e2289dba6c75e0
JuozasKaralius <106099015+JuozasKaralius@users.noreply.github.com>
2022-06-29 21:19:48 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+//  Navigation
+function toogleMenu () {
+    const toogleMenu = document.querySelector("toogleMenu");
+    const navigation = document.querySelector("toogleMenu");
+    toogleMenu.classList.toogle("active");
+    navigation.classList.toogle("active");
+
+}
+//  End Navigation
